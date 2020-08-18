beecf4168a58bd183af20f46029b86bff5733b4e
Adam Vadala-Roth <fenderstratman@gmail.com>
2020-08-18 03:24:38 -0400

    Last Edit
    Whatever the fuck I was doing here save it

--- vine-mbed/src/main.cpp
+++ vine-mbed/src/main.cpp
@@ -1,36 +1,43 @@
 #include <mbed.h>
+#include <BufferedSerial.h>
+#include <DigitalInOut.h>
 #include <USBCDC.h>
 
-int main() {
+// Standard Variable Declariations
+volatile uint8_t Relay_1_State = 0;
+volatile uint8_t Relay_2_State = 0;
+volatile uint8_t RS485_Port1_Term_State = 0;
+volatile uint8_t RS485_Port2_Term_State = 0;
 
+// UART Serial Objects
+volatile BufferedSerial SENSE_UART(PB_10, PC_5, MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
+volatile BufferedSerial RS485_ONE_UART(PA_0, PA_1, MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
+volatile BufferedSerial RS485_TWO_UART(PA_2, PA_3, MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
+
+int main()
+{
   // put your setup code here, to run once:
 
-  // Standard Variable Declariations
-  uint8_t Relay_1_State=0;
-  uint8_t Relay_2_State=0;
-  uint8_t RS485_Port1_Term_State = 0;
-  uint8_t RS485_Port2_Term_State = 0;
-  
-
   // USB VCOM Port Related Variables
-  unsigned int USB_COM_DATA[6] = {};                  // Data Array
-  uint8_t* p_USB_COM_DATA = (uint8_t*)USB_COM_DATA;   // write pointer
-  uint32_t USB_size_read;                             // the number of bytes actually received
+  unsigned int USB_COM_DATA[6] = {};                 // Data Array
+  uint8_t *p_USB_COM_DATA = (uint8_t *)USB_COM_DATA; // write pointer
+  uint32_t USB_size_read;                            // the number of bytes actually received
 
-  // Serial Object declarations 
-  // I2C Ports 
-  I2C i2c_IO_Expander(PA_8,PC_9);
-  I2C i2c_SENSE(PB_6,PB_7);
+  // Serial Object declarations
+  // I2C Ports
+  I2C i2c_IO_Expander(PA_8, PC_9);
+  I2C i2c_SENSE(PB_6, PB_7);
+  /*
   // UART Serial Objects
   BufferedSerial SENSE_UART(PB_10, PC_5, MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
   BufferedSerial RS485_ONE_UART(PA_0, PA_1, MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
-  BufferedSerial RS485_TWO_UART(PA_2,PA_3, MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
-  //USB Virtual COM Port 
+  BufferedSerial RS485_TWO_UART(PA_2, PA_3, MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
+  */
+  //USB Virtual COM Port
   USBCDC USP_Port();
 
-
   //IO Pin Declarations
-  //IO Expander control lines 
+  //IO Expander control lines
   DigitalInOut IO_RESET(PC_6);
   DigitalInOut IO_INT_N(PC_7);
   // I2C expander control line
@@ -52,21 +59,49 @@ int main() {
   DigitalInOut RS485_SL_RATE2(PA_7);
 
   // MAIN LOOP
-  while(1) {
+  while (1)
+  {
     // put your main code here, to run repeatedly:
-    
-  }// End Main While Loop
 
+  } // End Main While Loop
 
-
-
-}// end main
+} // end main
 
 // User Defined Functions
+
+// Calculate 8-bit CRC
+
+/*
+static byte crc8(const byte *addr, byte len)
+{
+}
+*/
+
+//Transmit a message
+void transmit(uint8_t data)
+{
+}
+
+// Receover a message
+uint8_t receive(uint8_t portNum)
+{
+}
+
+// Transmission complete
+void sendComplemented(const uint8_t what)
+{
+  
+}
+
+//set termination state of the RS485 port
+uint8_t RS485_Term_Set(uint8_t termState)
+{
+}
 
 // Selecting I2C mux output and control of sensors
 uint8_t I2C_SenseControl(uint8_t SensorPortNum)
 {
+
   return 0;
 }
 // Control of I2C GPIO expander
